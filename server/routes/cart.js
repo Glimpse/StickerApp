0c02a7fddcf1ed2ca80cf84efb18cfbd569dd4dd
chrisdias <cdias@microsoft.com>
2017-05-01 12:58:48 -0700

    'new' not needed when creating Router

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dataAccess = require('../db/data-access');
 
-const router = new express.Router();
+const router = express.Router();
 
 router.use(bodyParser.json());
 
