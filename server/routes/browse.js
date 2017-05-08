d3ee5a302d0ef42b47918929d6ce8c1ff6be2b49
chrisdias <cdias@microsoft.com>
2017-05-07 20:36:00 -0700

    build demo seed

--- server/routes/browse.js
+++ server/routes/browse.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = express.Router();
+const router = new express.Router();
 
 const dataAccess = require('../db/data-access');
 
