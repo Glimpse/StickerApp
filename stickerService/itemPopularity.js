29fa4294832d2b94fd1440575e04db40230f1528
Charles Lowell <chlowe@microsoft.com>
2017-06-07 12:49:48 -0700

    more flexible redis client configuration

--- stickerService/itemPopularity.js
+++ stickerService/itemPopularity.js
@@ -7,37 +7,16 @@
 // recent activity has more weight than past activity: scores decay over time.
 
 const events = require('events');
-const redis = require('redis');
 const db = require('./mongodb');
 const kafka = require('no-kafka');
+const redisClient = require('./redisClient');
+redisClient.on('ready', () => setInterval(decayScores, 60 * 1000));
+
+const REDIS_KEY = 'items:popularityscores';
+
 
 const eventEmitter = new events.EventEmitter();
 exports.on = (event, listener) => eventEmitter.on(event, listener);
-
-const REDIS_KEY = 'items:popularityscores';
-const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD || undefined,
-    tls: process.env.REDIS_TLS,
-    retry_strategy: options => {
-        // retry every 2 seconds for 10 seconds; crash, if that doesn't suffice
-        if (options.total_retry_time < 10 * 1000) {
-            console.log(`${options.error}, retrying...`);
-            return 2000;
-        }
-
-        throw `can't connect to redis`;
-    }
-});
-redisClient.on('error', err => {
-    // swallow errors to allow retries as specified in the retry_strategy above
-    console.log(`redis client error ${err}`);
-});
-redisClient.on('ready', () => {
-    console.log('redis client ready');
-    setInterval(decayScores, 60 * 1000);
-});
 
 exports.topKAsync = (k, withScores) => {
     // returns the top k highest-scoring items
