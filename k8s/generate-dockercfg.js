702ef849bc053617ad3420d5bbeeb8c622144916
Charles Lowell <chlowe@microsoft.com>
2017-06-14 15:59:05 -0700

    fix dockercfg script

--- k8s/generate-dockercfg.js
+++ k8s/generate-dockercfg.js
@@ -5,16 +5,15 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('registry name: ', registryName => {
+rl.question('registry (e.g. my-registry.azurecr.io): ', registry => {
     rl.question('username: ', username => {
         rl.question('password: ', password => {
-            dockercfg = {
-                registryName: {
-                    username,
-                    password,
-                    email: 'unused@acr.io',
-                    auth: new Buffer(`${username}:${password}`).toString('base64')
-                }
+            let dockercfg = {};
+            dockercfg[registry] = {
+                username,
+                password,
+                email: 'unused@acr.io',
+                auth: new Buffer(`${username}:${password}`).toString('base64')
             }
 
             const json = JSON.stringify(dockercfg);
